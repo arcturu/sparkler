f78104253cb44fda880d6695939d40ca80f6696b
arcturu <spinsota@gmail.com>
2017-06-21 13:49:47 +0900

    Implement shadow

--- src/raytrace.cc
+++ src/raytrace.cc
@@ -26,14 +26,36 @@ uint8_t saturate(double x, uint8_t limit) {
   }
 }
 
+Intersection getShadowIntersection(Scene& scene, Ray shadowRay, double maxdist) {
+  const double SHADOW_EPS = 1e-10;
+  Vector3d p = shadowRay.src;
+  Intersection it = scene.intersect(shadowRay);
+  while (it.hit && (it.p - p).length() < SHADOW_EPS) {
+    shadowRay.src = it.p + SHADOW_EPS * shadowRay.dir;
+    it = scene.intersect(shadowRay);
+  }
+  if ((it.p - p).length() > maxdist) {
+    it.hit = false;
+  }
+  return it;
+}
+
 // TODO: support other types of lights
-Pixel<uint8_t> shade(Intersection it, std::vector<Light> ls) {
+Pixel<uint8_t> shade(Scene& scene, Intersection it) {
   Pixel<uint8_t> pix;
-  for (const auto& l : ls) {
-    double dist = (l.p - it.p).length();
-    pix.r += saturate(l.color.r * it.n.dot(l.p - it.p) / 4.0 / M_PI / dist / dist * l.luminance * 6, 255);
-    pix.g += saturate(l.color.g * it.n.dot(l.p - it.p) / 4.0 / M_PI / dist / dist * l.luminance * 6, 255);
-    pix.b += saturate(l.color.b * it.n.dot(l.p - it.p) / 4.0 / M_PI / dist / dist * l.luminance * 6, 255);
+  for (const auto& l : scene.lights) {
+    Ray shadowRay;
+    shadowRay.src = it.p;
+    shadowRay.dir = l.p - it.p;
+    Intersection shadowIt = getShadowIntersection(scene, shadowRay, shadowRay.dir.length());
+    if (shadowIt.hit) {
+      pix.r = 0; pix.g = 0; pix.b = 0;
+    } else {
+      double dist = (l.p - it.p).length();
+      pix.r += saturate(l.color.r * it.n.dot(l.p - it.p) / 4.0 / M_PI / dist / dist * l.luminance * 6, 255);
+      pix.g += saturate(l.color.g * it.n.dot(l.p - it.p) / 4.0 / M_PI / dist / dist * l.luminance * 6, 255);
+      pix.b += saturate(l.color.b * it.n.dot(l.p - it.p) / 4.0 / M_PI / dist / dist * l.luminance * 6, 255);
+    }
   }
   return pix;
 }
@@ -49,7 +71,7 @@ Image<uint8_t> raytrace(Scene& scene) {
       Intersection it = scene.intersect(r);
       if (it.hit) {
 //        img.m[y][x].r = it.n.x() * 255; img.m[y][x].g = it.n.y() * 255; img.m[y][x].b = it.n.z() * 255;
-        img.m[y][x] = shade(it, scene.lights);
+        img.m[y][x] = shade(scene, it);
       } else {
         img.m[y][x].r = scene.camera.bgColor().r * 255;
         img.m[y][x].g = scene.camera.bgColor().g * 255;
